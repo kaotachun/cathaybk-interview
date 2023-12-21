335726bf23108311ba3e014ccd965c158dc150a0
高大淳 <bill@gaodachundeAir.lan>
2023-12-21 12:00:40 +0900

    logic question 1

--- logic_q1.py
+++ logic_q1.py
@@ -1,2 +1,12 @@
-for i in range(10):
-    print("hello world")
+class Solution(object):
+    def reverseNumber(self, nums):
+        new = []
+        for i in range(len(nums)):
+            temp = str(nums[i])
+            new.append(int(temp[1]+temp[0]))
+
+        return new
+
+if __name__ == '__main__':
+    a = Solution()
+    print(a.reverseNumber([53, 64, 75, 19, 92]))
